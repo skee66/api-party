1190f19ebca8225a6a307ee618cc5596f38e8ac4
Samuel Roberts <rober352@purdue.edu>
2017-05-30 15:43:07 -0400

    Add onChange handler

---
+++ src/Github.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import './Github.css'
+
+class Github extends Component {
+    state = {
+        username: ''
+    }
+
+    handleChange = (ev) => {
+        const username = ev.currentTarget.value
+        this.setState({ username })
+    }
+    render() {
+        return (
+            <div className='github'>
+                <img className='github' src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github" />
+                <form>
+                    <div>
+                        <input type="text" value={this.state.username} onChange={this.handleChange} />
+                    </div>
+                    <div>
+                        <button type='submit'>Look up Github user</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Github
