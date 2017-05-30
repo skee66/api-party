2c785e0cfd665968e7bbd79299da6a1de5b58a51
Samuel Roberts <rober352@purdue.edu>
2017-05-30 16:14:37 -0400

    Add user to state and render user details

--- src/Github.js
+++ src/Github.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import './Github.css'
+import { Route } from 'react-router-dom'
+import GithubUser from './GithubUser'
 
 class Github extends Component {
     state = {
@@ -10,11 +12,17 @@ class Github extends Component {
         const username = ev.currentTarget.value
         this.setState({ username })
     }
+
+    handleSubmit = (ev) => {
+        ev.preventDefault()
+        this.props.history.push(`/github/${this.state.username}`)
+    }
+
     render() {
         return (
             <div className='github'>
                 <img className='github' src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github" />
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <input type="text" value={this.state.username} onChange={this.handleChange} />
                     </div>
@@ -22,6 +30,11 @@ class Github extends Component {
                         <button type='submit'>Look up Github user</button>
                     </div>
                 </form>
+
+                <Route path='/github/:username' component={GithubUser} />
+                <Route exact path='/github' render={() => (
+                    <h3>Please enter a username to search on Github</h3>
+                )}/>
             </div>
         )
     }
